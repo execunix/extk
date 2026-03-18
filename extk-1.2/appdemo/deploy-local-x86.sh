73f0aabd5ba4fe50ceebc5c017772cfcfae9f386
execunix <execunix@gmail.com>
2026-03-18 22:27:04 +0900

    extk: add makefile for linux

---
+++ extk-1.2/appdemo/deploy-local-x86.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+readonly PROGRAM="$2"
+readonly TARGET_DIR="/mnt/work/tmp"
+#export DISPLAY=:10.0
+
+# Must match startsPattern in tasks.json
+echo "Deploying to target"
+
+# kill gdbserver on target and delete old binary
+/usr/bin/killall -q gdbserver; rm -rf ${TARGET_DIR}/${PROGRAM}; rm -rf /tmp/app.fifo
+
+# send the program to the target
+#cp ../libex2/bin/libgeminit-gn-gdc_x86.so ${TARGET_DIR}
+cp ${PROGRAM} ${TARGET_DIR}
+
+# Must match endsPattern in tasks.json
+echo "Starting GDB Server on Target"
+
+# start gdbserver on target
+cd ${TARGET_DIR}; gdbserver localhost:3000 ${PROGRAM}
